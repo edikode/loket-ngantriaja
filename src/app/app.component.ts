0eb9ee1d9b85163aa245c934bd5e2fa9c4231705
Edi Siswanto <edisiswanto.ti8.poliwangi@gmail.com>
2021-02-01 20:30:19 +0700

    add status connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogErrorComponent } from './components/dialog-error/dialog-error.component';
 import * as Recta from 'recta';
+import { ConnectionService } from 'ng-connection-service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,32 @@ export class AppComponent implements OnInit {
   title = 'mobile-ngantriaja';
   printer = new Recta(4228560628, 1811);
 
-  constructor(public dialog: MatDialog) {}
+  status = 'ONLINE';
+  isConnected = true;
+
+  constructor(
+    public dialog: MatDialog,
+    private connectionService: ConnectionService
+  ) {}
   ngOnInit() {
-    // this.openPrinter();
+    // check connection printer
+    this.openPrinter();
     this.checkConnection();
   }
 
   checkConnection() {
-    console.log('tes');
+    this.connectionService.monitor().subscribe((isConnected) => {
+      this.isConnected = isConnected;
+      if (this.isConnected) {
+        this.status = 'ONLINE';
+      } else {
+        this.status = 'OFFLINE';
+        this.openDialog(
+          'Anda sedang Offline. periksa kembali koneksi internet anda',
+          'koneksi'
+        );
+      }
+    });
   }
 
   openPrinter() {
@@ -31,17 +50,23 @@ export class AppComponent implements OnInit {
       .catch((e: any) => {
         // Show Error if get an Error
         this.openDialog(
-          'Printer belum terhubung, Periksa kembali koneksi printer anda'
+          'Printer belum terhubung, Periksa kembali koneksi printer anda',
+          'printer'
         );
       });
   }
 
-  openDialog(message: string) {
+  openDialog(message: string, mode: string) {
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.data = message;
+
+    let data = {
+      message: message,
+      mode: mode,
+    };
+    dialogConfig.data = data;
 
     const dialogRef = this.dialog.open(DialogErrorComponent, dialogConfig);
 
--- src/app/components/dialog-error/dialog-error.component.ts
+++ src/app/components/dialog-error/dialog-error.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConnectionService } from 'ng-connection-service';
 
 @Component({
   selector: 'app-dialog-error',
@@ -7,16 +8,33 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./dialog-error.component.scss'],
 })
 export class DialogErrorComponent implements OnInit {
+  status = 'ONLINE';
+  isConnected = true;
+
   constructor(
     public dialogRef: MatDialogRef<DialogErrorComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private connectionService: ConnectionService
   ) {}
 
   ngOnInit(): void {
     console.log(this.data);
+    if (this.data.mode == 'koneksi') {
+      this.checkConnection();
+    }
   }
 
   reload() {
     window.location.reload();
+  }
+
+  checkConnection() {
+    this.connectionService.monitor().subscribe((isConnected) => {
+      this.isConnected = isConnected;
+      if (this.isConnected) {
+        this.status = 'ONLINE';
+        this.dialogRef.close();
+      }
+    });
   }
 }
